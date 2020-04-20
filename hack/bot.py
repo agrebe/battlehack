15f49e4039d00a313e377a00b034ac824e9fc456
Gurtej Kanwar <gurtej@mit.edu>
2020-04-20 18:55:18 -0400

    Closer to an exploitable jump trick

--- hack/bot.py
+++ hack/bot.py
@@ -1,40 +1,46 @@
-def break_it():
-    a = 1
-    for r in [0]:
-        {}
-    for r in [0]:
-        {}
-        {}
-        {}
-        {}
-        {}
-        {}
-        {}
+# Need to use something other than blocks to ensure we have control of the stack
+# when we jump.
+# def break_it():
+#     a = 1
+#     for r in [0]:
+#         {}
+#     for r in [0]:
+#         {}
+#         {}
+#         {}
+#         {}
+#         {}
+#         {}
+#         {}
         
-    def dummy():
-        pass
-    def dummy2():
-        pass
-    def dummy3():
-        pass
-    def evil():
-        log("a")
-    evil()
+#     def dummy():
+#         pass
+#     def dummy2():
+#         pass
+#     def dummy3():
+#         pass
+#     def evil():
+#         log("a")
+#     evil()
 
-def break_it2():
-    a = 1
-    for r in [0]:
-        {}
-    for r in [0]:
-        {}
-        {}
-        {}
-        {}
-        {}
-        {}
-        {}
-    y = (lambda x: a)()
+# This almost works, but unclear how to construct C-backed code object in payload
+def call_code(payload):
+    a = True + (True if True or False else False)
+    {}
+    {}
+    {}
+    {}
+    a = payload + (True if True or False else False)
+    b = {}
+    b = b = b = b = b = {}
+    {}
+    {}
+    {}
+    {}
+    def evil():
+        pass
+    evil()
 
 def turn():
     # break_it()
-    break_it2()
+    call_code(b'')
