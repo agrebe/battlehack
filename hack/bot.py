f94f048a2d6f99782690ebe9b90a354e023e1351
Gurtej Kanwar <gurtej@mit.edu>
2020-04-19 23:54:05 -0400

    Adds partial sandbox escape hack

---
+++ hack/bot.py
@@ -0,0 +1,40 @@
+def break_it():
+    a = 1
+    for r in [0]:
+        {}
+    for r in [0]:
+        {}
+        {}
+        {}
+        {}
+        {}
+        {}
+        {}
+        
+    def dummy():
+        pass
+    def dummy2():
+        pass
+    def dummy3():
+        pass
+    def evil():
+        log("a")
+    evil()
+
+def break_it2():
+    a = 1
+    for r in [0]:
+        {}
+    for r in [0]:
+        {}
+        {}
+        {}
+        {}
+        {}
+        {}
+        {}
+    y = (lambda x: a)()
+
+def turn():
+    # break_it()
+    break_it2()
